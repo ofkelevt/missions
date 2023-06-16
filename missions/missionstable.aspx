65ee5aaf3f989d0aaa33384090f9f86cf1c05d4a
ofek levy <ievilootedo@outlook.com>
2023-06-16 23:51:48 +0300

    begingin

---
+++ missions/missionstable.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/master.Master" AutoEventWireup="true" CodeBehind="missionstable.aspx.cs" Inherits="missions.missionstable" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style>
+        table,td,tr,th{border:solid black 2px; }
+   
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <form method ="post">
+        <%=tablex %>
+    </form>
+    <table>
+        <tr>
+            <td>
+                <form method ="post">
+                    <input type="submit" name="submit" value ="submit text" />
+                </form>
+            </td>
+            <td>
+                <form>
+                    <input type ="submit" name="addr" value="add row" />
+                </form>
+            </td>
+            <td>
+                <form>
+                    <input type ="submit" name="addl" value="add lengh" />
+                </form>
+            </td>
+            
+        </tr>
+   </table>
+   
+</asp:Content>
---
+++ missions/missionstable.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace missions
+{
+    public partial class missionstable : System.Web.UI.Page
+    {
+        public int row=1 ;
+        public int lengh=1;
+        public string[,] data = new string[20, 20];
+        public string tablex ;
+        public int saver;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (Request["addr"] != null)
+            {
+                if (Session["addr"] == null)
+                    row++;
+                else row = Convert.ToInt16(Session["addr"]);
+            }
+            if (Request["addl"] != null)
+            {
+                lengh++;
+            }
+            if(row != 1)
+            {
+                Session["addr"] = row;
+            }
+
+            tablex += "<table>";
+            for(int i = 0; i<row +1; i++)
+            {
+                tablex += "<tr>" ;
+                for (int j = 0; j < lengh; j++)
+                {
+                    if (i == 0) { tablex += "<th>" + $"title<input type = \"text\" name =\" title{j}\" " + "</th>"; }
+                    else tablex += "<td>" + $"mission<input type = \"text\" name =\" text{i}.{j}\" " + "</td>";
+                }
+                tablex += "</tr>";
+            }
+            tablex += "</table>";
+           
+        }
+    }
+}
